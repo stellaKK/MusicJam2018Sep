336713a23c416f8fef45fa31c40ab3127b97e258
Yuan Wei <raynorwei@gmail.com>
2018-09-08 13:37:56 -0400

    added fonts assets/ added music

---
+++ Assets/Scripts/GroundBreak.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GroundBreak : MonoBehaviour {
+
+    public float FadeInTime;
+
+    GameObject ground1;
+    GameObject ground2;
+    GameObject ground3;
+
+    bool isBreaking1 = false;
+    bool isBreaking2 = false;
+    bool isBreaking3 = false;
+
+    bool hasSelected = false;
+    bool hasReseted = false;
+
+    float startBreak;
+
+    Color oldColor;
+
+    // Use this for initialization
+    void Start () {
+        ground1 = this.transform.GetChild(0).gameObject;
+        ground2 = this.transform.GetChild(1).gameObject;
+        ground3 = this.transform.GetChild(2).gameObject;
+        oldColor = ground1.GetComponent<SpriteRenderer>().color;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (hasSelected == false)
+        {
+           hasSelected = true;
+           SelectBlock();
+        }
+
+        if (isBreaking1)
+        {
+            BreaksBlock(ground1);
+        }
+
+        if (isBreaking2)
+        {
+            BreaksBlock(ground2);
+        }
+
+        if (isBreaking3)
+        {
+            BreaksBlock(ground3);
+        }
+    }
+
+    void SelectBlock() {
+        int i;
+        i = Random.Range(0, 3);
+        if (i == 0) {
+            startBreak = Time.time;
+            ground1.GetComponent<SpriteRenderer>().color = Color.red;
+            isBreaking1 = true;
+        }
+        else if (i == 1)
+        {
+            startBreak = Time.time;
+            ground2.GetComponent<SpriteRenderer>().color = Color.red;
+            isBreaking2 = true;
+        }
+        else if (i == 2)
+        {
+            startBreak = Time.time;
+            ground3.GetComponent<SpriteRenderer>().color = Color.red;
+            isBreaking3 = true;
+        }
+        Invoke("ResetSelection", FadeInTime + 6f);
+    }
+
+    void ResetSelection() {
+        hasSelected = false;
+    }
+
+    void BreaksBlock(GameObject block) {
+        print("is breaking");
+        SpriteRenderer fadePanel = block.GetComponent<SpriteRenderer>();
+        Color currentColor = fadePanel.color;
+
+        if (Time.time - startBreak < FadeInTime)
+        {
+            float alphaChange = Time.deltaTime / FadeInTime;
+            currentColor.a -= alphaChange;
+            fadePanel.color = currentColor;
+        } else {
+            block.SetActive(false);
+            isBreaking1 = false;
+            isBreaking2 = false;
+            isBreaking3 = false;
+            Invoke("ResetBlock", FadeInTime);
+        }
+    }
+
+    void ResetBlock() {
+        ground1.GetComponent<SpriteRenderer>().color = oldColor;
+        ground2.GetComponent<SpriteRenderer>().color = oldColor;
+        ground3.GetComponent<SpriteRenderer>().color = oldColor;
+        print("changing color to white");
+        ground1.SetActive(true);
+        ground2.SetActive(true);
+        ground3.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/QuestsManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class QuestsManager : MonoBehaviour {
+
+    GameObject quest1;
+    GameObject quest2;
+    GameObject quest3;
+
+    Button quest1Button;
+    Button quest2Button;
+    Button quest3Button;
+
+    // Use this for initialization
+    void Start () {
+        quest1Button = GameObject.Find("Quest1").GetComponent<Button>();
+        quest2Button = GameObject.Find("Quest2").GetComponent<Button>();
+        quest3Button = GameObject.Find("Quest3").GetComponent<Button>();
+        quest1 = GameObject.Find("Quest1Information");
+        quest2 = GameObject.Find("Quest2Information");
+        quest3 = GameObject.Find("Quest3Information");
+
+        quest1.SetActive(false);
+        quest2.SetActive(false);
+        quest3.SetActive(false);
+
+        DisableButtons();
+        EnableButtons();
+        
+
+        
+    }
+
+    public void DisableButtons() {
+        quest1Button.interactable = false;
+        quest2Button.interactable = false;
+        quest3Button.interactable = false;
+    }
+
+    public void EnableButtons() {
+        quest1Button.interactable = true;
+
+        if (PlayerPrefs.GetInt("Quest2Unlocked") == 1)
+        {
+            quest2Button.interactable = true;
+        }
+
+        if (PlayerPrefs.GetInt("Quest3Unlocked") == 1)
+        {
+            quest2Button.interactable = true;
+        }
+    }
+
+    public void SelectQuest1() {
+        quest1.SetActive(true);
+        DisableButtons();
+    }
+
+    public void SelectQuest2() {
+        quest2.SetActive(true);
+        DisableButtons();
+    }
+
+    public void SelectQuest3()
+    {
+        quest3.SetActive(true);
+        DisableButtons();
+    }
+
+    public void AcceptQuest(string questName) {
+        EnableButtons();
+        quest1.SetActive(false);
+        quest2.SetActive(false);
+        quest3.SetActive(false);
+        SceneManager.LoadScene(questName);
+    }
+
+    public void DeclineQuest() {
+        quest1.SetActive(false);
+        quest2.SetActive(false);
+        quest3.SetActive(false);
+        EnableButtons();
+    }
+
+}
