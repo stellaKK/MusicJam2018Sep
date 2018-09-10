e5c2a5004327f16e9b7d4dca03cb382b3353198e
Yuan Wei <raynorwei@gmail.com>
2018-09-10 11:43:29 -0400

    Structure

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -39,24 +39,24 @@ public class GameManager : MonoBehaviour {
     int leftNextIndex = 0;
     int rightNextIndex = 0;
 
-    float[] l1UpNotes = {14.05f, 16.25f, 24.4f, 26.2f };
-    float[] l1LeftNotes = { 3.5f, 5.7f, 8f, 9.1f, 10.2f, 11.3f, 12.4f, 13.5f, 14.6f, 15.7f, 16.8f, 17.9f, 18.95f, 19.8f, 20.8f, 21.7f, 22.6f, 23.5f, 24.4f, 25.3f, 26.2f, 27.1f, 28f, 28.9f, 29.8f, 30.7f, 31.6f };
-    float[] l1RightNotes = { 4.6f, 6.8f, 8.55f, 9.65f, 10.75f, 11.85f, 12.95f, 14.05f, 15.15f, 16.25f, 17.35f, 18.40f, 19.45f, 20.35f, 21.25f, 22.15f, 23.05f, 23.95f, 24.85f, 25.75f, 26.65f, 27.55f, 28.45f, 29.35f, 30.25f, 31.15f, 32.05f };
-    float[] l2UpNotes = { };
-    float[] l2LeftNotes = { };
-    float[] l2RightNotes = { };
-    float[] l3UpNotes = { };
-    float[] l3LeftNotes = { };
-    float[] l3RightNotes = { };
+    float[] l1UpNotesList = {14.05f, 16.25f, 24.4f, 26.2f };
+    float[] l1LeftNotesList = { 3.5f, 5.7f, 8f, 9.1f, 10.2f, 11.3f, 12.4f, 13.5f, 14.6f, 15.7f, 16.8f, 17.9f, 18.95f, 19.8f, 20.8f, 21.7f, 22.6f, 23.5f, 24.4f, 25.3f, 26.2f, 27.1f, 28f, 28.9f, 29.8f, 30.7f, 31.6f };
+    float[] l1RightNotesList = { 4.6f, 6.8f, 8.55f, 9.65f, 10.75f, 11.85f, 12.95f, 14.05f, 15.15f, 16.25f, 17.35f, 18.40f, 19.45f, 20.35f, 21.25f, 22.15f, 23.05f, 23.95f, 24.85f, 25.75f, 26.65f, 27.55f, 28.45f, 29.35f, 30.25f, 31.15f, 32.05f };
+    float[] l2UpNotesList = { };
+    float[] l2LeftNotesList = { };
+    float[] l2RightNotesList = { };
+    float[] l3UpNotesList = { };
+    float[] l3LeftNotesList = { };
+    float[] l3RightNotesList = { };
 
 
     int index = 0;
 
     // Use this for initialization
     void Start() {
-        PlayerPrefs.SetInt("L1Notes", l1UpNotes.Length + l1LeftNotes.Length + l1RightNotes.Length);
-        PlayerPrefs.SetInt("L2Notes", l1UpNotes.Length + l1LeftNotes.Length + l1RightNotes.Length);
-        PlayerPrefs.SetInt("L3Notes", l1UpNotes.Length + l1LeftNotes.Length + l1RightNotes.Length);
+        PlayerPrefs.SetInt("L1Notes", l1UpNotesList.Length + l1LeftNotesList.Length + l1RightNotesList.Length);
+        PlayerPrefs.SetInt("L2Notes", l1UpNotesList.Length + l1LeftNotesList.Length + l1RightNotesList.Length);
+        PlayerPrefs.SetInt("L3Notes", l1UpNotesList.Length + l1LeftNotesList.Length + l1RightNotesList.Length);
 
         maxCombo = -1;
         character = GameObject.Find("Character").GetComponent<Character>();
@@ -79,6 +79,13 @@ public class GameManager : MonoBehaviour {
         dsptimesong = (float)AudioSettings.dspTime;
         Debug.Log("secPerBeat: " + secPerBeat);
         Debug.Log("dsptimesong: " + dsptimesong);
+
+        upIndex = 0;
+        leftIndex = 0;
+        rightIndex = 0;
+        upNextIndex = 0;
+        leftNextIndex = 0;
+        rightNextIndex = 0;
     }
 
     // Update is called once per frame
@@ -94,7 +101,7 @@ public class GameManager : MonoBehaviour {
         //    PlayerPrefs.SetString("CurrentScene", SceneManager.GetActiveScene().name);
         //    PlayerPrefs.SetString("SongName", music.clip.name);
         //    PlayerPrefs.SetInt("Combo", maxCombo);
-            
+
         //    SceneManager.LoadScene("Result");
         //}
 
@@ -118,18 +125,35 @@ public class GameManager : MonoBehaviour {
 
         songPosition = (float)AudioSettings.dspTime - dsptimesong;
         songPosInBeats = (float)Math.Round((songPosition / secPerBeat), 1);
-
-        if (upNextIndex < l1UpNotes.Length && l1UpNotes[upNextIndex] < songPosInBeats)
+        if (SceneManager.GetActiveScene().name == "Level1")
         {
-            if (leftNextIndex < l1LeftNotes.Length || rightNextIndex < l1RightNotes.Length) {
-                if (l1UpNotes[upNextIndex] == l1LeftNotes[leftNextIndex])
+            SpawningNotes(l1UpNotesList, l1LeftNotesList, l1RightNotesList);
+        }
+        else if (SceneManager.GetActiveScene().name == "Level2")
+        {
+            SpawningNotes(l2UpNotesList, l2LeftNotesList, l2RightNotesList);
+        }
+        else if (SceneManager.GetActiveScene().name == "Level3"){
+            SpawningNotes(l3UpNotesList, l3LeftNotesList, l3RightNotesList);
+        }
+
+        
+
+    }
+
+    void SpawningNotes(float[] upNotes, float[] LeftNotes, float[] rightNotes) {
+        if (upNextIndex < upNotes.Length && upNotes[upNextIndex] < songPosInBeats)
+        {
+            if (leftNextIndex < LeftNotes.Length || rightNextIndex < rightNotes.Length)
+            {
+                if (upNotes[upNextIndex] == LeftNotes[leftNextIndex])
                 {
                     upNoteSpawner.SpawnDoubleNote();
                     upNextIndex++;
                     leftNoteSpawner.SpawnDoubleNote();
                     leftNextIndex++;
                 }
-                else if (l1UpNotes[upNextIndex] == l1RightNotes[rightNextIndex])
+                else if (upNotes[upNextIndex] == rightNotes[rightNextIndex])
                 {
                     upNoteSpawner.SpawnDoubleNote();
                     upNextIndex++;
@@ -141,22 +165,25 @@ public class GameManager : MonoBehaviour {
                     upNoteSpawner.SpawnSingleNote();
                     upNextIndex++;
                 }
-            } else {
+            }
+            else
+            {
                 upNoteSpawner.SpawnSingleNote();
                 upNextIndex++;
             }
         }
-        if (leftNextIndex < l1LeftNotes.Length && l1LeftNotes[leftNextIndex] < songPosInBeats)
+        if (leftNextIndex < LeftNotes.Length && LeftNotes[leftNextIndex] < songPosInBeats)
         {
-            if (upNextIndex < l1UpNotes.Length || rightNextIndex < l1RightNotes.Length) {
-                if (l1LeftNotes[leftNextIndex] == l1UpNotes[upNextIndex])
+            if (upNextIndex < upNotes.Length || rightNextIndex < rightNotes.Length)
+            {
+                if (LeftNotes[leftNextIndex] == upNotes[upNextIndex])
                 {
                     leftNoteSpawner.SpawnDoubleNote();
                     leftNextIndex++;
                     upNoteSpawner.SpawnDoubleNote();
                     upNextIndex++;
                 }
-                else if (l1LeftNotes[upNextIndex] == l1RightNotes[rightNextIndex])
+                else if (LeftNotes[upNextIndex] == rightNotes[rightNextIndex])
                 {
                     leftNoteSpawner.SpawnDoubleNote();
                     leftNextIndex++;
@@ -168,26 +195,27 @@ public class GameManager : MonoBehaviour {
                     leftNoteSpawner.SpawnSingleNote();
                     leftNextIndex++;
                 }
-            } else
+            }
+            else
             {
                 leftNoteSpawner.SpawnSingleNote();
                 leftNextIndex++;
             }
 
-            
+
         }
-        if (rightNextIndex < l1RightNotes.Length && l1RightNotes[rightNextIndex] < songPosInBeats)
+        if (rightNextIndex < rightNotes.Length && rightNotes[rightNextIndex] < songPosInBeats)
         {
-            if (upNextIndex < l1UpNotes.Length || leftNextIndex < l1LeftNotes.Length)
+            if (upNextIndex < upNotes.Length || leftNextIndex < LeftNotes.Length)
             {
-                if (l1RightNotes[leftNextIndex] == l1UpNotes[upNextIndex])
+                if (rightNotes[leftNextIndex] == upNotes[upNextIndex])
                 {
                     rightNoteSpawner.SpawnDoubleNote();
                     rightNextIndex++;
                     upNoteSpawner.SpawnDoubleNote();
                     upNextIndex++;
                 }
-                else if (l1RightNotes[rightNextIndex] == l1LeftNotes[upNextIndex])
+                else if (rightNotes[rightNextIndex] == LeftNotes[upNextIndex])
                 {
                     rightNoteSpawner.SpawnDoubleNote();
                     rightNextIndex++;
@@ -200,12 +228,12 @@ public class GameManager : MonoBehaviour {
                     rightNextIndex++;
                 }
             }
-            else {
+            else
+            {
                 rightNoteSpawner.SpawnSingleNote();
                 rightNextIndex++;
             }
         }
-
     }
 
     public void RecordPosition(string name) {
